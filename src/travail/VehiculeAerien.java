7752299669b7400560afdc06f1da657b13d21d7a
StraetenMax <maxstraeten@yahoo.com>
2017-05-22 09:46:17 +0200

    préparation class VehiculeAerien

--- src/travail/VehiculeAerien.java
+++ src/travail/VehiculeAerien.java
@@ -1,5 +1,35 @@
 package travail;
 
-public class VehiculeAerien {
+public class VehiculeAerien extends Vehicule {
+	//propriétés
+	protected int rayonAction;
+	protected boolean longCourrier;
+	
+	//constructeur vide et initialisé
+	public VehiculeAerien(){
+		rayonAction = 0;
+		this.setLongCourrier();
+	}
+	public VehiculeAerien(int rayonAction){
+		this.rayonAction= rayonAction;
+		this.setLongCourrier();
+	}
 
+	//accesseurs et mutateurs
+	public int getRayonAction(){
+		return rayonAction;
+	}
+	public boolean getLongCourrier(){
+		return longCourrier;	
+	}
+	public void setRayonAction(int rayonAction){
+		this.rayonAction= rayonAction;
+	}
+	public void setLongCourrier(){
+		if(this.getRayonAction()>5){
+			this.longCourrier= true;
+		}else{
+			this.longCourrier= false;
+		}
+	}
 }
