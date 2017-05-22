973c44afba2338d37c00b2830cdd5d6f0b0b598f
StraetenMax <maxstraeten@yahoo.com>
2017-05-22 11:07:18 +0200

    class Avion

--- src/travail/Avion.java
+++ src/travail/Avion.java
@@ -1,5 +1,30 @@
 package travail;
 
 public class Avion {
+	//propriétés
+	private String compagnie;
+	private boolean militaire;
+	
+	//constructeurs vide et initialisé
+	public Avion(){
+		super();
+		compagnie = "";
+		militaire = true;
+	}
+	
+	//accesseurs et mutateurs
+	public String getCompagnie(){
+		return compagnie;
+	}
+	public boolean getMilitaire(){
+		return militaire;
+	}
+	public void setCompagnie(String compagnie){
+		this.compagnie=compagnie;
+	}
+	public void setMilitaire(boolean compagnie){
+		this.militaire = militaire;
+	}
+	
 
 }
