9aa551fa3c5e285ddf9d205e21f4805da08a6dd0
StraetenMax <maxstraeten@yahoo.com>
2017-05-22 13:19:47 +0200

    class Auto méthodes

--- src/travail/Auto.java
+++ src/travail/Auto.java
@@ -1,5 +1,33 @@
 package travail;
 
-public class Auto {
+public class Auto extends VehiculeTerrien {
+	//propriétés
+	private int nbPorte;
+	
+	//constructeurs vide et initialisé
+	public Auto(){
+		super();
+		nbPorte = 0;
+	}
+	public Auto(String couleur, int nbOccupants, int nbPorte){
+		super(couleur, nbOccupants);
+		this.nbPorte = nbPorte;
+	}
+	
+	//accesseur et mutateur
+	public int getNbPorte(){
+		return nbPorte;
+	}
+	public void setNbPorte(int nbPorte){
+		this.nbPorte= nbPorte;
+	}
+	
+	//méthodes
+	public void avancer(){
+		System.out.println("J'avance en Auto");
+	}
+	public void seGarer(){
+		System.out.println("Je me gare en Auto");
+	}
 
 }
