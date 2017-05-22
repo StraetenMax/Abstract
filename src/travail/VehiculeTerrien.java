bb0e0ff044750b7eabc6dd39fe5da090d945e0e0
StraetenMax <maxstraeten@yahoo.com>
2017-05-22 10:08:22 +0200

    class VehiculeTerrien

--- src/travail/VehiculeTerrien.java
+++ src/travail/VehiculeTerrien.java
@@ -1,5 +1,22 @@
 package travail;
 
-public class VehiculeTerrien {
-
+public class VehiculeTerrien extends Vehicule {
+	//propriétés
+	
+	//constructeurs vide et initialisé
+	public VehiculeTerrien(){
+		super();
+	}
+	public VehiculeTerrien(String couleur, int nbOccupants){
+		super(couleur, nbOccupants);
+	}
+	
+	//methodes
+	public void avancer(){
+		System.out.println("J’avance terrien");
+	}
+	public void seGarer(){
+		System.out.println("Je me gare terrien");
+	}
+	
 }
